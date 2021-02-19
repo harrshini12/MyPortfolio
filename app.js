87a98c03f71c9debc168303b4b0edb6b4732ea92
harrshini12 <harrshini.1995@gmail.com>
2021-02-19 11:09:09 +0100

    portfolioup

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-const express = require("express")
+const express = require("express");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
 app.listen(process.env.PORT || 3000, 
 () => console.log("Server is running..."));
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+  
+transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+  app.post("/send", (req, res) => {
+    
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      console.log(fields);
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+
+      const mail = {
+        from: data.name,
+        to: process.env.EMAIL,
+        subject: data.subject,
+        text: `${data.name} <${data.email}> \n${data.message}`,
+      };
+  
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Something went wrong.");
+        } else {
+          res.status(200).send("Email successfully sent to recipient!");
+        }
+      });
+    });
+  });
--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
 AOS.init();
+
+const form = document.getElementById("contact-form"); 
+
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let mail = new FormData(form);
+  sendMail(mail);
+})
+const sendMail = (mail) => {
+    fetch("https://harrshini-portfolio.herokuapp.com/send", {
+      method: "post", 
+      body: mail, 
+  
+    }).then((response) => {
+      return response.json();
+    });
+  };
+
