62e4014766dfb8af3fe201855a769fcf3061c0a7
harrshini12 <harrshini.1995@gmail.com>
2021-03-11 13:01:50 +0100

    contactpageupdate

--- public/index.js
+++ public/index.js
@@ -2,25 +2,6 @@
 AOS.init();
 //-----End of Animation-----//
 
-//-----Form-----//
-const form = document.getElementById("contact-form"); 
-
-const formEvent = form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let mail = new FormData(form);
-  sendMail(mail);
-})
-const sendMail = (mail) => {
-    fetch("https://harrshini-portfolio.herokuapp.com/send", {
-      method: "post", 
-      body: mail, 
-  
-    }).then((response) => {
-      return response.json();
-    });
-  };
-//-----End of Form-----//
-
 //-----Back to top-----//
 
 //Get the button
